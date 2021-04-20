9a55c4f50e9fb8b26932e2f81d6d96f2cebaebd4
Caio Cichetti <caio_cichetti@outlook.com>
2021-04-20 18:42:57 -0300

    feat: added design patterns

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello World!',
-  });
-});
+app.get('/', (req, res) => res.json({
+  message: 'Hello World!',
+}));
 
-app.post('/route', (req, res) => {
-  return res.json({
-    message: 'The requisition has arrived!',
-  });
-});
+app.post('/route', (req, res) => res.json({
+  message: 'The requisition has arrived!',
+}));
 
 app.listen('3333', () => console.log('Server is running 🚀'));
